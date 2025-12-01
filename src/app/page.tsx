63fecbf28d7fe8c99b756feb194c61c8d346adf3
Vader1970 <daniel.wilkey@gmail.com>
2025-12-01 18:26:01 +1300

    checking repository is correctly connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Multi Vendor Ecommerce</h1>
+      <h1 className="text-3xl font-bold underline">Multi Vendor Ecommerce Initializing...</h1>
     </div>
   );
 }
